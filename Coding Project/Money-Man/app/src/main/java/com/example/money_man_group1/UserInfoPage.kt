390e3b5cc93709342f429761e682eb02ea178919
Your Name <you@example.com>
2024-09-18 18:22:13 -0500

    made userInfo screen and save button takes you to budget page (EDITED main activty button to go to user info(FOR NOW))

--- Coding Project/Money-Man/app/src/main/java/com/example/money_man_group1/UserInfoPage.kt
+++ Coding Project/Money-Man/app/src/main/java/com/example/money_man_group1/UserInfoPage.kt
@@ -8,22 +8,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class UserInfoPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_user_info_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val loginButton = findViewById<Button>(R.id.loginScreenButton)
-        loginButton.setOnClickListener {
-            val intent = Intent(this, UserInfoPage::class.java)
+        val userInfoButton = findViewById<Button>(R.id.userInfoButton) //creates save button
+        userInfoButton.setOnClickListener {
+            val intent = Intent(this, BudgetPage::class.java) //when clicked user info page goes to budget page
             startActivity(intent)
         }
-
     }
 }
