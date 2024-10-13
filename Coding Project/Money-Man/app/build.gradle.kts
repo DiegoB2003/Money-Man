b970fab93310e153d62c8e70ac5c8d2387a365bd
Nickick10 <filipov.nick@gmail.com>
2024-10-13 16:47:42 -0500

    Fixed minSdk version to make app run with firebase

--- Coding Project/Money-Man/app/build.gradle.kts
+++ Coding Project/Money-Man/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.money_man_group1"
-        minSdk = 19
+        minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
