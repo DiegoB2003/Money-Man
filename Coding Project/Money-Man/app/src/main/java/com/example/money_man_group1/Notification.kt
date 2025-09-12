47b20de3a14e0556cbfa7e7a24363fbd8da11d1a
DiegoB2003 <bravod427@gmail.com>
2025-09-12 02:08:05 -0500

    reformatted folders

--- Coding Project/Money-Man/app/src/main/java/com/example/money_man_group1/Notification.kt
+++
@@ -1,7 +0,0 @@
-package com.example.money_man_group1
-
-data class Notification(
-    val message: String = "",
-    val timestamp: Long = System.currentTimeMillis(),
-    val isRead: Boolean = false
-)
--- Coding Project/Money-Man/app/src/main/java/com/example/money_man_group1/userActivityLog.kt
+++
@@ -1,8 +0,0 @@
-package com.example.money_man_group1
-
-data class UserActivityLog(
-    val message: String = "",
-    val amount: Double = 0.0,
-    val category : String = "",
-    val timestamp: Long = System.currentTimeMillis()
-)
